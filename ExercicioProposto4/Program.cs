322b0458bb6f6294ba3b9c4701e01d7740302bdb
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-26 20:31:48 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class ExercicioProposto4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Id Funcionario: ");
+            int funcionarioId = int.Parse(Console.ReadLine());
+            Console.Write("Horas Trabalhadas: ");
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+            Console.Write("Valor por Hora: ");
+            double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario = horasTrabalhadas * valorPorHora;
+            Console.WriteLine($"NUMBER = {funcionarioId}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+
+        }
+    }
+}
